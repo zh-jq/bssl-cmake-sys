267cd7f9d6b683b9e600bde1ef80873a40375ab7
Zhang Jingqiang <zh_jq@outlook.com>
2024-12-07 14:57:09 +0800

    ignore clippy warning

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::ptr_offset_with_cast)]
+#![allow(clippy::useless_transmute)]
 
 use core::ffi::c_ulong;
 
